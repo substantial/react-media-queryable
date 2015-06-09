cdee165ae25815f4e589cf14f35334730affa26f
James Lee <james@substantial.com>
2015-06-09 16:24:25 -0700

    (wip) Add umd wrapper

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gutil = require('gulp-util');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var reactify = require('reactify');
+var umd = require('gulp-umd');
 
 gulp.task('javascript', function () {
   var b = browserify({
@@ -21,8 +22,27 @@ gulp.task('javascript', function () {
     .pipe(source('media_queryable.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(uglify())
-      .on('error', gutil.log)
+    .on('error', gutil.log)
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('umd', ['javascript'], function() {
+  return gulp.src('dist/media_queryable.js')
+    .pipe(umd({
+      exports: function(file) {
+        return "MediaQueryable";
+      },
+      dependencies: function(file) {
+        return [
+          {
+            name: 'react',
+            param: 'React'
+          }
+        ];
+      }
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['umd']);
---
+++ src/media_queryable.js
@@ -0,0 +1,40 @@
+var React = require('react');
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      mediaQuery: undefined
+    };
+  },
+
+  componentDidMount: function() {
+    for (name in this.props.mediaQueries) {
+      this._setupMediaQuery(name);
+    }
+  },
+
+  render: function() {
+    if (this.state.mediaQuery == undefined) {
+      return null;
+    }
+
+    var renderedChildren = React.Children.map(this.props.children, function(child) {
+      return React.cloneElement(child, { mediaQuery: this.state.mediaQuery } );
+    }.bind(this));
+    return React.DOM.div({children: renderedChildren});
+  },
+
+  _setupMediaQuery: function(name) {
+    var mql = window.matchMedia(this.props.mediaQueries[name]);
+    mql.addListener((function(e) {
+      this._handleMediaQueryChange(e.matches, name);
+    }).bind(this));
+
+    this._handleMediaQueryChange(mql.matches, name);
+  },
+
+  _handleMediaQueryChange: function(matches, name) {
+    if (matches) {
+      this.setState({mediaQuery: name});
+    }
+  }
+});
