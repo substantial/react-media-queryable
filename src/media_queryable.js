6baa7b5fd1d3b3513deb7e61809d9a32ad7d2df2
James Lee <james@substantial.com>
2015-06-09 10:17:23 -0700

    Convert MediaQueryable to return component

--- src/media_queryable.js
+++ src/media_queryable.js
@@ -1,31 +1,59 @@
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['React', 'MediaListener'], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory(require('React'), require('MediaListener'));
-  } else {
-    root.MediaQueryable = factory(root.React, root.MediaListener);
-  }
-}(this, function (React, MediaListener) {
-  var MediaQueryableMixin = function(mediaQueries) {
-    return {
-      getInitialState: function() {
-        return {
-          mediaQuery: undefined
-        };
-      },
+var React = require('react');
 
-      componentDidMount: function() {
-        return new MediaListener(mediaQueries, this._onMediaQueryChange);
-      },
+var MediaQueryable = function(mediaQueries) {
+  var MediaQueryableComponent = React.createClass({
+    displayName: "MediaQueryable",
 
-      _onMediaQueryChange: function(name) {
-        return this.setState({
-          mediaQuery: name
-        });
+    getInitialState: function() {
+      return {
+        mediaQuery: undefined
+      };
+    },
+
+    childContextTypes: {
+      mediaQuery: React.PropTypes.string
+    },
+
+    getChildContext: function() {
+      return {
+        mediaQuery: this.state.mediaQuery
       }
-    };
-  };
+    },
 
-  return MediaQueryableMixin;
-}));
+    componentDidMount: function() {
+      for (name in mediaQueries) {
+        this._setupMediaQuery(name);
+      }
+    },
+
+    render: function() {
+      if (this.state.mediaQuery == undefined) {
+        return null;
+      }
+
+      console.log("rendering media queryable");
+      return <div>
+        { this.props.children }
+      </div>;
+    },
+
+    _setupMediaQuery: function(name) {
+      var mql = window.matchMedia(mediaQueries[name]);
+      mql.addListener((function(e) {
+        this._handleMediaQueryChange(e.matches, name);
+      }).bind(this));
+
+      this._handleMediaQueryChange(mql.matches, name);
+    },
+
+    _handleMediaQueryChange: function(matches, name) {
+      if (matches) {
+        this.setState({mediaQuery: name});
+      }
+    }
+  });
+
+  return MediaQueryableComponent;
+};
+
+module.exports = MediaQueryable;
