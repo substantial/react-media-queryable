ecb8ee9c14cfe15f0acd691e64bb6561c6b802a7
James Lee <james@substantial.com>
2015-06-10 11:21:11 -0700

    Remove old umd wrapper

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// (function (root, factory) {
-//   if (typeof define === 'function' && define.amd) {
-//     define(['react'], factory);
-//   } else if (typeof exports === 'object') {
-//     module.exports = factory(require('react'));
-//   } else {
-//     root.MediaQueryable = factory(root.React);
-//   }
-// }(this, function (React) {
-
-// }));
-
 require('./src/media_queryable');
